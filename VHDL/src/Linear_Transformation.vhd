d8a230ac2b25cc6235b3b13f2c64177e64b2676c
creignje <creignou.e1903202@etud.univ-ubs.fr>
2020-03-30 21:00:46 +0200

    Linear Transformation

---
+++ VHDL/src/Linear_Transformation.vhd
@@ -0,0 +1,10 @@
+LIBRARY IEEE;
+
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY Linear_Transformation IS
+END Linear_Transformation;
+
+ARCHITECTURE Behavioral OF Linear_Transformation IS
+BEGIN
+END Behavioral;
