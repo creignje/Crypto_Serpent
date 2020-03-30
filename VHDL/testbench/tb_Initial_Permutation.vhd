801521fb224310e7ed55f629370d39c810c5f9ed
creignje <creignou.e1903202@etud.univ-ubs.fr>
2020-03-30 21:02:29 +0200

    testbench Initial Permutation

--- VHDL/testbench/tb_Initial_Permutation.vhd
+++ VHDL/testbench/tb_Initial_Permutation.vhd
@@ -1,8 +1,33 @@
 LIBRARY IEEE;
 
+USE IEEE.STD_LOGIC_1164.ALL;
+
 ENTITY tb_Initial_Permutation IS
 END tb_Initial_Permutation;
 
 ARCHITECTURE Behavioral OF tb_Initial_Permutation IS
+
+COMPONENT Initial_Permutation
+    PORT(data_in: IN STD_LOGIC_VECTOR(127 DOWNTO 0);
+         data_out: OUT STD_LOGIC_VECTOR(127 DOWNTO 0));
+END COMPONENT;
+
+-- Signal Reporting
+
+SIGNAL s_data_in: STD_LOGIC_VECTOR(127 DOWNTO 0);
+SIGNAL s_data_out: STD_LOGIC_VECTOR(127 DOWNTO 0);
+
 BEGIN
+
+dut : Initial_Permutation
+
+PORT MAP(
+    data_in => s_data_in,
+    data_out => s_data_out
+);
+
+PROCESS
+BEGIN
+    
+END PROCESS;
 END Behavioral;
