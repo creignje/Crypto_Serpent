2e095161198a9b16cec4d46eafc54d50b0653aa3
creignje <creignou.e1903202@etud.univ-ubs.fr>
2020-03-30 15:09:43 +0200

    Sbox1 Update

--- VHDL/src/Sbox1.vhd
+++ VHDL/src/Sbox1.vhd
@@ -17,6 +17,23 @@ BEGIN
 
 -- Sbox initialization
 
+s_sbox(0) <= "1111";	-- 15
+s_sbox(1) <= "1100";	-- 12
+s_sbox(2) <= "0010";	-- 2
+s_sbox(3) <= "0111";	-- 7
+s_sbox(4) <= "1001";	-- 9
+s_sbox(5) <= "0000";	-- 0
+s_sbox(6) <= "0101";	-- 5
+s_sbox(7) <= "1010";	-- 10
+s_sbox(8) <= "0001";	-- 1
+s_sbox(9) <= "1011";	-- 11
+s_sbox(10) <= "1110";	-- 14
+s_sbox(11) <= "1000";	-- 8
+s_sbox(12) <= "0110";	-- 6
+s_sbox(13) <= "1101";	-- 13
+s_sbox(14) <= "0011";	-- 3
+s_sbox(15) <= "0100";	-- 4
+
 -- Reading data
 
 data_out <= s_sbox(TO_INTEGER(UNSIGNED(data_in)));
