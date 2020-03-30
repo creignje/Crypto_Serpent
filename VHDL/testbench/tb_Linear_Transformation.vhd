5a073fb9e17781a3064aa281f14772799346d719
creignje <creignou.e1903202@etud.univ-ubs.fr>
2020-03-30 21:03:00 +0200

    testbench Linear Transformation

---
+++ VHDL/testbench/tb_Linear_Transformation.vhd
@@ -0,0 +1,10 @@
+LIBRARY IEEE;
+
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY tb_Linear_Transformation IS
+END tb_Linear_Transformation;
+
+ARCHITECTURE Behavioral OF tb_Linear_Transformation IS
+BEGIN
+END Behavioral;
